3e030158442f56459eadd980a099b5cafa974b5f
muhammadawaisansari <muhammadawaisansari529@gmail.com>
2023-03-01 13:49:38 +0500

    name change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Olx
+          Welcome to My Olx App
         </p>
       </header>
     </div>
